a95b6c14c424c684dacad1e1734aa4a79becdf49
marcymarcy <marcy@realeyes.com>
2018-04-20 20:07:57 -0600

    First working version - takes in JSON and returns JSON

--- README.md
+++ README.md
@@ -5,3 +5,20 @@ Provide packages and versions, and get back features and bugs.
 docker build --file BuildtestDockerfile --tag ironsalsa/get-features:latest .
 
 docker run --mount type=bind,source="$(pwd)",target=/testapp ironsalsa/get-features:latest
+
+# Input format
+
+```
+{
+    "dependencyversions": [
+        {
+            "version": "2.0.3",
+            "package": "@scope/packagename"
+        },
+        {
+            "version": "2.0.24",
+            "package": "unscopedpackage"
+        }
+    ]
+}
+```
--- get-features.py
+++ get-features.py
@@ -1,17 +1,27 @@
 import argparse
 import json
-from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search
-esclient = Elasticsearch()
+import requests
 
 # Gather CLI arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--json", help="Path to JSON file with packages and versions")
 parser.add_argument("--index", help="ElasticSearch index to search")
-#parser.add_argument("--yaml", help="Path to YAML file with packages and versions")
 parser.add_argument("-u", "--url", help="URL for ElasticSearch server to query")
+parser.add_argument("--doctype", help="ElasticSearch doc type to search. (default: doc)", default="doc")
 parser.add_argument("-v", "--verbose", action="store_true", help="Give more verbose output")
 args = parser.parse_args()
+
+# Define a function to remove duplicates
+def Remove(duplicate):
+    final_list = []
+    for num in duplicate:
+        if num not in final_list:
+            final_list.append(num)
+    return final_list
+
+# Declare the lists we need to return
+feature_list = []
+bug_list = []
 
 # Import JSON
 with open(args.json, 'rb') as json_data:
@@ -23,12 +33,19 @@ only_keyvalue = parsed_json['dependencyversions']
 for stuff in only_keyvalue:
     pack_version.update({stuff["package"]:stuff["version"]})
 
-# Poll ElasticSearch for data
+# Get back features and bugs from ElasticSearch 6.2 for all packages
+for pack,vers in pack_version.items():
+    # Make an API request for ES data
+    r = requests.get(args.url + "/" + args.index + "/" + args.doctype + "/_search?default_operator=AND&q=" + "name:" + "\"" + pack + "\"" + "+" + "version:" + vers)
+    j = json.loads(r.text)
+    # Navigate the nested JSON to get the data we want and add to list...check that number if anything breaks
+    feature_list.extend(j['hits']['hits'][0]['_source']['featuretotal'])
+    bug_list.extend(j['hits']['hits'][0]['_source']['bugfixtotal'])
 
+# Remove duplicates
+bugs = Remove(bug_list)
+features = Remove(feature_list)
 
-# Outputs
-if args.json:
-    print(json.dumps(pack_version))
-#    print(type(parsed_json))
-if args.url:
-    print(args.url)
+# Create a dictionary for the 2 lists and return as JSON
+combined_json = { "featuretotal": features, "bugfixtotal": bugs }
+print(json.dumps(combined_json))
